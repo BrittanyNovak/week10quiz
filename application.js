c2ffac2bdbbeef4406b23a91488c32d61c91c227
BrittanyNovak <brittanynicole0106@gmail.com>
2015-08-18 19:29:39 -0500

    getting it working

--- application.js
+++ application.js
@@ -2,4 +2,13 @@ function myFunction(){
 	var one = document.getElementById("one");
 	var two = document.getElementById("two");
 	var output = document.getElementById("output");
+	
+	for(var j = 1; j <= two.value; j++){
+		output.innerHTML += "<div>";
+	
+		for(var i = 1; i <= one.value; i++){
+			output.innerHTML += i + " ";
+		}
+		output.innerHTML += "</div>";
+	}
 }
